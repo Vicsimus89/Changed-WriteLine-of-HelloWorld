888dcbb95d11a10897e2f6232f9d383f40862659
Vicsimus89 <fastpitching06801@msn.com>
2023-07-10 00:31:28 -0600

    created a console app project

---
+++ Variables_Exercise1/Variables_Exercise1/Program.cs
@@ -0,0 +1,14 @@
+﻿// Declare and initialize variables
+string name = "Jimmy";
+int age = 45;
+char middleInitial = 'L';
+bool isAVeteran = true;
+double currentYearsExperience = 11.5;
+decimal currentStartingHourlyPay = 42.95m;
+
+Console.WriteLine($"Name: {name}\n" +
+                  $"Age: {age}\n" +
+                  $"Middle Initial: {middleInitial}\n" +
+                  $"Is A Veteran: {isAVeteran}\n" +
+                  $"Current Years Experience: {currentYearsExperience}\n" +
+                  $"Current Starting Hourly Pay: {currentStartingHourlyPay}\n");
